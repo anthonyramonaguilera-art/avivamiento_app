aefdc863e0056d9959fc78409622002cd8faaf88
Anthony Ramon Aguilera <anthonyramonaguilera@gmail.com>
2025-11-17 05:25:41 -0400

    busqueda del problema en el bakend de la radio

--- android/app/src/main/kotlin/com/example/flutter_application_respaldo/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-// Archivo dejado como respaldo; no declara la misma clase ni paquete
-package com.example.flutter_application_respaldo
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class RespaldoMainActivity: FlutterFragmentActivity() {
-}
